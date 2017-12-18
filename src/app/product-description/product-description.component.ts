89e12454acab7c858755973fa2535e2ec8c4e88c
akshaymehtani <akshay.mehtani@gmail.com>
2017-12-18 11:51:18 -0500

    Re-commit

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,9 +8,10 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
+  albumInfo;
   constructor(private _productService: ProductService) { }
 
-  albumInfo;
+  
   ngOnInit() {
     this._productService.getAlbum(1)
       .subscribe(response => this.albumInfo = response);
